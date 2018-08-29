840dd6b52475e9202aa88c8a5a1212623c271a0a
Brian W <39372876+BRWelk@users.noreply.github.com>
2018-08-29 13:20:54 -0500

    Lunch time
    Set up test for async

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,20 +1,38 @@
 describe('Address Book', function () {
+  var addressBook,
+  thisContact; //declaring variables for the suite. Inner scopes have access to outer scopes.
+
+  beforeEach(function() { //setting up to instantiate before each it (test).
+    addressBook = new AddressBook();
+    thisContact = new Contact();
+  });
+
     it('should be able to add a contact', function() {
-        var addressBook = new AddressBook(),
-        thisContact = new Contact(); //declaring variables for the suite. Inner scopes have access to outer scopes.
 
         addressBook.addContact(thisContact);
 
-        expect(addressBook.getContact(0)).toBe(thisContact);
+          expect(addressBook.getContact(0)).toBe(thisContact);
     });
 
     it('should be able to delete a contact', function() {
-        var addressBook = new AddressBook(),
-        thisContact = new Contact(); //declaring variables for the suite. Inner scopes have access to outer scopes.
 
         addressBook.addContact(thisContact);
         addressBook.deleteContact(0);
 
-        expect(addressBook.getContact(0)).not.toBeDefined();                   
+          expect(addressBook.getContact(0)).not.toBeDefined();
     });
+});
+
+describe('Async Address Book', function() {
+  var addressBook = new AddressBook;
+
+  beforeEach(function(done) {
+    addressBook.getInitialContacts(function() {
+          done();
+    });
+  });
+  it('should grab inital contacts', function(done) {
+    expect(addressBook.initialComplete).toBe(true);
+    done();
+  });
 });
