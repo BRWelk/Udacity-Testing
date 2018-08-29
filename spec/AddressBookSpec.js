069b5187e241002edb51f2377f39cb2e0824ea0e
bwelk <bwelk66@gmail.com>
2018-08-29 06:36:12 -0500

    practiced

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,26 +1,20 @@
 describe('Address Book', function () {
-    var addressBook,
-        thisContact; //declaring variables for the suite. Inner scopes have access to outer scopes.
-
-    beforeEach(function() {
-        var addressBook = new AddressBook(); //instantiate addressBook and thisContact variables
-        thisContact = new Contact();
-    });
-
-    beforeEach();
-    
     it('should be able to add a contact', function() {
+        var addressBook = new AddressBook(),
+        thisContact = new Contact(); //declaring variables for the suite. Inner scopes have access to outer scopes.
 
         addressBook.addContact(thisContact);
 
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
 
-    it('should be able to delete contact', function() {
+    it('should be able to delete a contact', function() {
+        var addressBook = new AddressBook(),
+        thisContact = new Contact(); //declaring variables for the suite. Inner scopes have access to outer scopes.
 
-        addressBook.addContact(thisContact);//add contact to address book so...
-        addressBook.deleteContact(0);//...it can be deleted
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
 
-        expect(addressBook.getContact(0)).not.toBeDefined(); /*(THE TEST)I expect to get contact from address book and for it not to be defined.*/
+        expect(addressBook.getContact(0)).not.toBeDefined();                   
     });
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -3,13 +3,13 @@ function AddressBook() {
 }
 
 AddressBook.prototype.addContact = function(contact) {
-    this.contacts.push(contact);//pushing a 'contact(name)' to the array when added (.addContact)
+    this.contacts.push(contact);
 }
 
 AddressBook.prototype.getContact = function(index) {
-    return this.contacts[index];//function to create index var in the contacts array. It puts a var to the list of contacts, so they might be
+    return this.contacts[index];
 }
 
 AddressBook.prototype.deleteContact = function(index) {
-    this.contacts.splice(index, 1);/*using splice of an array to move the element. Index is the items in the contacts [] array (from line 2). index is set in line 9 at the .getContact function and returns index to line 2, this.contacts*/
+    this.contacts.splice(index, 1);
 }
